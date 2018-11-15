2f4a03834e341293ecf6ca221d499598a3cb4fbf
Johannes <j.fusenig@gmx.net>
2018-11-15 15:48:53 +0100

    Added two Querrys

--- src/main/java/de/hhu/propra/db/data/KundeRepository.java
+++ src/main/java/de/hhu/propra/db/data/KundeRepository.java
@@ -3,6 +3,12 @@ package de.hhu.propra.db.data;
 import de.hhu.propra.db.entities.Kunde;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface KundeRepository extends CrudRepository<Kunde, Long> {
+
+    List<Kunde> findTop10By();
+
+    List<Kunde> findByAutosFarbe(String farbe);
 
 }
